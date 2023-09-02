e1e81e4d22ab1443d6df718f239bdaa84d971312
cvicens <carlos.vicens.alonso@gmail.com>
2023-09-02 08:57:18 +0200

    check oc token and fixes env var expansion in .env

--- run-dev.sh
+++ run-dev.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-export $(grep -v '^#' .env | xargs)
+OC_USER=$(oc whoami)
+OC_TOKEN=$(oc whoami -t)
+
+if [ -z "${OC_TOKEN}" ]
+then
+    echo "You have to log in the OpenShift cluster and have cluster-admin permissions"
+    exit 1
+fi
+
+# export $(grep -v '^#' .env | xargs)
+export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 
 echo "DEV_USERNAME=${DEV_USERNAME}"
 
